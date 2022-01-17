5274fd503fc2d509bffd2103840fa9ca6146cf4f
grzeznx2 <grzeznx2@gmail.com>
2022-01-17 11:49:23 +0100

    create redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import 'antd/dist/antd.css'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
+import store from './app/store.js'
+import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 )
