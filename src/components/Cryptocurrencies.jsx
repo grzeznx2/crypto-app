9a3de4c2296c0a40fbb8fcc712a1736fdae46439
grzeznx2 <grzeznx2@gmail.com>
2022-01-17 17:59:39 +0100

    fetch and display cryptocurrencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { Card, Col, Row } from 'antd'
+import millify from 'millify'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
-const Cryptocurrencies = () => {
-  return <div>Cryptocurrencies</div>
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+  if (isFetching) return <>Loading...</>
+
+  return (
+    <>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map(({ id, rank, name, iconUrl, price, marketCap, change }) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={id}>
+            <Link to={`/crypto/${id}`}>
+              <Card
+                title={`${rank}. ${name}`}
+                extra={<img className="crypto-image" src={iconUrl} alt={name} />}
+                hoverable
+              >
+                <p>Price: {millify(price)}</p>
+                <p>Market Cap: {millify(marketCap)}</p>
+                <p>Daily Change: {millify(change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
 
 export default Cryptocurrencies
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,11 +3,13 @@ import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import { Cryptocurrencies, News } from '.'
 
 const { Title } = Typography
 
 const HomePage = () => {
-  const { data, isFetching } = useGetCryptosQuery()
+  const { data, isFetching } = useGetCryptosQuery(10)
+  console.log(data)
 
   const globalStats = data?.data?.stats
 
@@ -35,6 +37,24 @@ const HomePage = () => {
           <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
     </div>
   )
 }
