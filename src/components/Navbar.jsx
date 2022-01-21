8f08728c7508bbcfb5caed747f5fa54e4b8d81f9
grzeznx2 <grzeznx2@gmail.com>
2022-01-21 01:22:35 +0100

    finish Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Avatar, Button, Menu, Typography } from 'antd'
 import {
@@ -11,6 +11,27 @@ import {
 import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true)
+  const [screenSize, setScreenSize] = useState(undefined)
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth)
+
+    window.addEventListener('resize', handleResize)
+
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    if (screenSize <= 1000) {
+      setActiveMenu(false)
+    } else {
+      setActiveMenu(true)
+    }
+  }, [screenSize])
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -18,22 +39,26 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">CryptoNews</Link>
         </Typography.Title>
+        <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
-      <Button className="menu-control-container"></Button>
+      {activeMenu && (
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   )
 }
